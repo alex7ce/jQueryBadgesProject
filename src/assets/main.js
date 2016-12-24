7ab2af144c0be1cfe6902205bef156210f6faec3
alex7ce <af@acesofthouse.com>
2016-12-24 16:21:09 +0200

    Completed project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
-
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2827214.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      $.each(response.courses.completed, function(index, value) {
+        $('#badges').append('<div class="course">'
+        + '<h3>' + value.title + '</h3>'
+        + '<img src="' + value.badge + '"/>'
+        + '<a class="btn btn-primary" target="_blank" href="' + value.url + '">See course</a>'
+        + '</div>');
+      });
+    }
+  });
 
 });
